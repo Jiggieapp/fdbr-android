60e2cf982ae046fbe1eec8ec38eaed5d6ae29a7b
aga42 <itu.aga42@gmail.com>
2016-08-31 16:40:27 +0700

    profile UI(2) ripple

---
+++ app/src/main/java/com/fdbr/android/view/widget/NonSwipeableViewPager.java
@@ -0,0 +1,46 @@
+package com.fdbr.android.view.widget;
+
+import android.content.Context;
+import android.support.v4.view.ViewPager;
+import android.util.AttributeSet;
+import android.view.MotionEvent;
+
+/**
+ * Created by LTE on 8/31/2016.
+ */
+public class NonSwipeableViewPager extends ViewPager {
+
+    private Boolean enabled;
+
+    public NonSwipeableViewPager(Context context) {
+        super(context);
+        this.enabled = false;
+    }
+
+    public NonSwipeableViewPager(Context context, AttributeSet attrs) {
+        super(context, attrs);
+        this.enabled = false;
+    }
+
+    @Override
+    public boolean onTouchEvent(MotionEvent event) {
+        if (this.enabled) {
+            return super.onTouchEvent(event);
+        }
+        return false;
+    }
+
+    @Override
+    public boolean onInterceptTouchEvent(MotionEvent event) {
+        if (this.enabled) {
+            return super.onInterceptTouchEvent(event);
+        }
+
+        return false;
+    }
+
+    public void setPagingEnabled(boolean enabled) {
+        this.enabled = enabled;
+    }
+
+}
